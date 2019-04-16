1a4351b6af24255d8eb1e33e4f237e0eee725af9
Amy Thompson <amythompson9@gmail.com>
2019-04-16 00:04:49 +0000

    New Work

--- RestfulRouting/RESTfulBlogApp/app.js
+++ RestfulRouting/RESTfulBlogApp/app.js
@@ -23,6 +23,8 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res){
     res.redirect("/blogs");
 });
+
+//INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -33,6 +35,28 @@ app.get("/blogs", function(req, res){
     });
 });
 
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res) {
+   //create blog
+   Blog.create(req.body.blog, function(err, newBlog) {
+       if(err) {
+           res.render("new");
+       } else {
+           //then, redirect to the index
+           res.redirect("/blogs");
+       }
+   });
+});
+
+//SHOW Route
+app.get("/blogs/:id", function(req, res){
+   res.send("SHOW PAGE!"); 
+});
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING");
 })
