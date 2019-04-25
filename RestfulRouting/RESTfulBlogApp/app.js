f8902535975d839289ac73d394e4c78776aa655d
Amy Thompson <amythompson9@gmail.com>
2019-04-25 00:12:58 +0000

    Update and Edit

--- RestfulRouting/RESTfulBlogApp/app.js
+++ RestfulRouting/RESTfulBlogApp/app.js
@@ -1,4 +1,5 @@
 var bodyParser  = require("body-parser"),
+methodOverride  = require("method-override"),
 mongoose        = require("mongoose"),
 express         = require("express"),
 app             = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost:27017/restful_blog-app", {useNewUrlParser:
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -63,6 +65,34 @@ app.get("/blogs/:id", function(req, res){
            res.render("show", {blog: foundBlog});
        }
    }); 
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//Update Route
+app.put("/blogs/:id", function(req, res){
+    //this takes three arguments 
+    //the id to find by
+    //the new data
+    //the callback
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
