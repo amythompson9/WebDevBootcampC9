84a7c7d792e5a36d7bff09b6314a7f0952c15345
Amy Thompson <amythompson9@gmail.com>
2019-04-25 00:30:36 +0000

    Delete - not working

--- RestfulRouting/RESTfulBlogApp/app.js
+++ RestfulRouting/RESTfulBlogApp/app.js
@@ -95,6 +95,20 @@ app.put("/blogs/:id", function(req, res){
     });
 });
 
+//Delete Route
+app.delete("/blogs/:id", function(req, res){
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.redirect("/blogs");
+        }
+    })
+    //redirect somewhere
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING");
 })
