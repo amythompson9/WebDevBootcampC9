1059879f7b3b5bea7e0d0fee4d171721fd266f22
Amy Thompson <amythompson9@gmail.com>
2019-06-16 00:17:11 +0000

    User auth part 2

--- YelpCamp/v6/app.js
+++ YelpCamp/v6/app.js
@@ -114,6 +114,30 @@ app.post("/campgrounds/:id/comments", function(req, res){
    //redirect campground show page
 });
 
+// =====================
+//AUTH ROUTES
+//======================
+
+//show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+//handle sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res. redirect("/campgrounds");
+        });
+    });
+    //User.register(new User({username: req.body.username}))
+});
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started")
 });
