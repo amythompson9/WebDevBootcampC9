44399452dcdec105045b3560fcfc0de474cfdea6
AmyT <amythompson9@gmail.com>
2019-07-09 00:19:56 +0000

    A bunch of npm install

--- YelpCamp/v11/app.js
+++ YelpCamp/v11/app.js
@@ -49,5 +49,5 @@ app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The YelpCamp Server Has Started")
+    console.log("The YelpCamp Server Has Started");
 });
